11d7eac74bf2ed2e4d075051bffd0b0f84fba79c
xorangeday <40214697+xorangeday@users.noreply.github.com>
2018-09-09 23:08:01 +0800

    Add files via upload

---
+++ LegitInventorySystem/InventorySystemDesign/InventorySystemDesign/FileMaintenancePage.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InventorySystemDesign
+{
+    /// <summary>
+    /// Interaction logic for FileMaintenancePage.xaml
+    /// </summary>
+    public partial class FileMaintenancePage : Page
+    {
+        public FileMaintenancePage()
+        {
+            InitializeComponent();
+            List<Branch> branch = new List<Branch>();
+            branch.Add(new Branch()
+            {
+                branch_code = 1,
+                branch_acronym = "HO",
+                branch_name = "HEAD OFFICE"
+            });
+
+            branch.Add(new Branch()
+            {
+                branch_code = 1,
+                branch_acronym = "HO",
+                branch_name = "HEAD OFFICE"
+            });
+
+            branch.Add(new Branch()
+            {
+                branch_code = 1,
+                branch_acronym = "HO",
+                branch_name = "HEAD OFFICE OF THE PRESIDENT DUTERTE WITH HIS SECRETARY KITTY"
+            });
+
+            branch.Add(new Branch()
+            {
+                branch_code = 1,
+                branch_acronym = "HO",
+                branch_name = "HEAD OFFICE"
+            });
+
+            branch.Add(new Branch()
+            {
+                branch_code = 1,
+                branch_acronym = "HO",
+                branch_name = "HEAD OFFICE OF T"
+            });
+
+            branch.Add(new Branch()
+            {
+                branch_code = 1,
+                branch_acronym = "HO",
+                branch_name = "HEAD OFFICE O"
+            });
+            branch.Add(new Branch()
+            {
+                branch_code = 1,
+                branch_acronym = "HO",
+                branch_name = "HEAD OFFICE OF"
+            });
+            branch.Add(new Branch()
+            {
+                branch_code = 1,
+                branch_acronym = "HO",
+                branch_name = "HEAD OFFICE OF"
+            });
+            branch.Add(new Branch()
+            {
+                branch_code = 1,
+                branch_acronym = "HO HO OH OHO OH OHO HO OH OH",
+                branch_name = "HEAD OFFICE OF"
+            });
+            branch.Add(new Branch()
+            {
+                branch_code = 1,
+                branch_acronym = "HO",
+                branch_name = "HEAD OFFICE OF"
+            });
+            branchList.ItemsSource = branch;
+        }
+        public class Branch
+        {
+            public int branch_code { get; set; }
+
+            public string branch_name { get; set; }
+
+            public string branch_acronym { get; set; }
+        }
+    }
+}
---
+++ LegitInventorySystem/InventorySystemDesign/InventorySystemDesign/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InventorySystemDesign
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButtonMenu_Click(object sender, RoutedEventArgs e)
+        {
+            OpenButtonMenu.Visibility = Visibility.Visible;
+            CloseButtonMenu.Visibility = Visibility.Collapsed;
+            logo1.Visibility = Visibility.Hidden;
+            logo.Visibility = Visibility.Visible;
+        }
+
+        private void OpenButtonMenu_Click(object sender, RoutedEventArgs e)
+        {
+            OpenButtonMenu.Visibility = Visibility.Collapsed;
+            CloseButtonMenu.Visibility = Visibility.Visible;
+            logo.Visibility = Visibility.Hidden;
+            logo1.Visibility = Visibility.Visible;
+        }
+
+        private void ListViewItem_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+        private void DashboardBT_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Home.Content = new DashboardPage();
+        }
+
+        private void FileMaintenanceBTN_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Home.Content = new FileMaintenancePage();
+        }
+
+        private void InventoryBTN_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Home.Content = new InventoryPage();
+        }
+
+        private void ReportBTN_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Home.Content = new ReportPage();
+        }
+
+        private void AccountsBTN_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Home.Content = new AccountsPage();
+        }
+
+
+        
+
+    }
+}
